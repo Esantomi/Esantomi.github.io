15f8cc9d664a5f8769d3fe9bc3c0a907912dafa2
Esantomi <pilinnasa@gmail.com>
2022-03-31 20:48:11 +0900

    test

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,23 @@
+export const environment = {
+  production: true,
+  // replace the data below with your personal data
+  personal: {
+    caricature: "haein-Illustration.png",
+    name: "Haein Ji",
+    birth: "1994-01-15",
+    email: "haein@aks.ac.kr",
+    phone: "+82 10 0000 0000",
+    location: "Seoul, South Korea"
+  },
+  // replace the dummy data below with the real firebase configs
+  firebaseConfig: {
+    apiKey: "AIzaSyC2frVRAAltsVTmdwgFnATGHoVUzNHfzsM",
+    authDomain: "live-resume-a575a.firebaseapp.com",
+    databaseURL: "https://live-resume-a575a.firebaseio.com",
+    projectId: "live-resume-a575a",
+    storageBucket: "live-resume-a575a.appspot.com",
+    messagingSenderId: "681076751855",
+    appId: "1:681076751855:web:18bae3866ebfcc4fcd8a1a",
+    measurementId: "G-00VXD77WNG"
+  }
+};
